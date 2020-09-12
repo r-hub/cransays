1d25476ea555e6c01155dddfd25822839e7e164a
Hadley Wickham <h.wickham@gmail.com>
2020-09-12 09:57:32 -0500

    Add basic analysis script

---
+++ analysis.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+raw <- vroom::vroom(dir(pattern = "*.csv"))
+
+tidy <- raw %>%
+  filter(!is.na(version)) %>%
+  transmute(package = paste0(package, "-", version), folder, snapshot_time) %>%
+  arrange(package, snapshot_time)
+
+# Remove packages that appear in multiple folders
+dups <- tidy %>% count(package, snapshot_time) %>% filter(n > 1)
+tidy <- tidy %>% anti_join(dups, by = c("package", "snapshot_time"))
+
+# remove all entries that didn't change
+state <- tidy %>%
+  group_by(package) %>%
+  filter(row_number() == 1 | folder != dplyr::lag(folder)) %>%
+  mutate(elapsed = as.numeric(snapshot_time - dplyr::lag(snapshot_time), units = "hours"))
+
+state %>% ungroup() %>% count(folder)
+
+state %>%
+  mutate(cur = folder, prev = dplyr::lag(folder)) %>%
+  ungroup() %>%
+  filter(!is.na(prev)) %>%
+  group_by(prev, cur) %>%
+  summarise(n = n(), elapsed = mean(elapsed), .groups = "drop")
